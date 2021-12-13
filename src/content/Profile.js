e20878506a033c43f57d6b3247a34c85b3509511
ksower <namkey4717@gmail.com>
2021-12-14 04:30:26 +0900

    'portfolio'

--- src/content/Profile.js
+++ src/content/Profile.js
@@ -29,18 +29,18 @@ function Profile(props){
 
                 <div className='prof-exp'>
                     <div {...animatedItem[1]}>
-                        <h2>Shopify App</h2>
-                        세계에서 가장 빠른 속도로 성장하는 Shopify의 Sales and Conversion 카테고리에 Super Social Proof, Retargeting In Store 두 종류의 앱을 출시 하였습니다.
+                        <h2>프로그램 설계자</h2>
+                        세미나와 코딩 아카데미를 참여하여 프로그램에 대한 기본적인 지식을 공부하였고, 아이디어 공모전 혹은 프로젝트를 진행하면서 프로그램을 설계하는 법을 익혔습니다.
                     </div>
 
                     <div {...animatedItem[2]}>
-                        <h2>Shopify App</h2>
-                        세계에서 가장 빠른 속도로 성장하는 Shopify의 Sales and Conversion 카테고리에 Super Social Proof, Retargeting In Store 두 종류의 앱을 출시 하였습니다.
+                        <h2>한 명의 연구자</h2>
+                        1년 동안 연구실에서 일을 하며 아이디어 회의 및 개발 작업을 진행하였으며, YOLO와 LSTM등의 인공지능 모델을 사용하여 실제 프로젝트를 진행한 경험이 있습니다.
                     </div>
 
                     <div {...animatedItem[3]}>
-                        <h2>Shopify App</h2>
-                        세계에서 가장 빠른 속도로 성장하는 Shopify의 Sales and Conversion 카테고리에 Super Social Proof, Retargeting In Store 두 종류의 앱을 출시 하였습니다.
+                        <h2>나아가는 개발자</h2>
+                        하고싶은 일을 하고, 만들고 싶은 것을 만들기 위해 지속적으로 세미나에 참여하여 최근 개발 지식을 습득하고 발전시켜 제 역량을 향상시킬 것입니다.
                     </div>
                 </div>
 
