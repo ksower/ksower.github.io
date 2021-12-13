31640d384abc6c2b4efa6b2b3a266b7da347f0fa
ksower <namkey4717@gmail.com>
2021-12-13 17:11:20 +0900

    'portfolio'

--- src/content/Project.js
+++ src/content/Project.js
@@ -9,29 +9,13 @@ function Project(props){
     const [menu, setMenu] = useState('All');
     const animatedItem = {
         0: useScrollFadeIn('down', 1, 0.5),
-        1: useScrollFadeIn('up', 1, 0.6)
+        1: useScrollFadeIn('up', 1, 0.6),
+        2: useScrollFadeIn('up', 1, 0.6)
     };
 
     function goGithubLink(_link){
         window.open(_link);
     }
-
-    const proj_list = Dataset_proj.map((item, index) => {
-        const img_src = './project-images/' + item.image;
-
-        return(
-            <figure class="proj hover">
-                <img src={require(`${img_src}`).default}/>
-                <figcaption>
-                    <p>The inside of my head was exploding with fireworks. Fortunately, my last thought turned out the lights when it left.</p>
-                    <div class="heading">
-                    <h2>{item.title}</h2>
-                    </div>
-                </figcaption>
-                <a href={`${item.link}`}></a>
-            </figure>
-        )
-    });
 
     return(
         <div className='Project'>
@@ -42,8 +26,28 @@ function Project(props){
                     "저는 이런 것들을 만들었습니다."
                 </div>
 
-                <div className='proj-area' {...animatedItem[1]}>
-                    {proj_list}
+                <div className='proj-area'>
+                    <figure class="proj hover" {...animatedItem[1]}>
+                        <img src={require(`${'./project-images/' + Dataset_proj[0].image}`).default}/>
+                        <figcaption>
+                            <p>The inside of my head was exploding with fireworks. Fortunately, my last thought turned out the lights when it left.</p>
+                            <div class="heading">
+                            <h2>{Dataset_proj[0].title}</h2>
+                            </div>
+                        </figcaption>
+                        <a href={`${Dataset_proj[0].link}`}></a>
+                    </figure>
+
+                    <figure class="proj hover" {...animatedItem[2]}>
+                        <img src={require(`${'./project-images/' + Dataset_proj[1].image}`).default}/>
+                        <figcaption>
+                            <p>The inside of my head was exploding with fireworks. Fortunately, my last thought turned out the lights when it left.</p>
+                            <div class="heading">
+                            <h2>{Dataset_proj[1].title}</h2>
+                            </div>
+                        </figcaption>
+                        <a href={`${Dataset_proj[1].link}`}></a>
+                    </figure>
                 </div>
 
                 <div className='proj-empty2'></div>
--- src/hooks/useScrollFadeIn.js
+++ src/hooks/useScrollFadeIn.js
@@ -37,7 +37,7 @@ const useScrollFadeIn = (direction = 'up', duration = 1, delay = 0) => {
         let observer;
 
         if (element.current) {
-        observer = new IntersectionObserver(onScroll, { threshold: 0.7 });
+        observer = new IntersectionObserver(onScroll, { threshold: 0.6 });
         observer.observe(element.current);
         }
 
